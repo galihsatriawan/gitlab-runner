2b8331da5f657b5f2e4ffc19455659191a9365f0
galihsatriawan <satriawan.galih99@gmail.com>
2020-03-11 13:40:45 +0700

    setup

---
+++ setup.sh
@@ -0,0 +1,9 @@
+docker run -d --name gitlab-runner-config \
+    -v /etc/gitlab-runner \
+    busybox:latest \
+    /bin/true
+\
+docker run -d --name gitlab-runner --restart always \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    --volumes-from gitlab-runner-config \
+    gitlab/gitlab-runner:latest
