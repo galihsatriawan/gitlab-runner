9a7901e2b144dd792900ab757bcf7fe25380e267
Galih Anggi Satriawan (galihsatriawan) <satriawan.galih99@gmail.com>
2020-11-06 16:38:51 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,4 +6,5 @@ docker run -d --name gitlab-runner-config \
 docker run -d --name gitlab-runner --restart always \
     -v /var/run/docker.sock:/var/run/docker.sock \
     --volumes-from gitlab-runner-config \
+    -v /usr/bin/docker:/usr/bin/docker \
     gitlab/gitlab-runner:latest
